8dfcde7295b4c30318e66c5778039b78b9156b62
Abhishek <7005093380a@gmail.com>
2024-07-06 14:57:05 +0530

    Completed

---
+++ README.md
@@ -0,0 +1,69 @@
+# CHIP-8 Interpreter in Python
+
+This project is a CHIP-8 interpreter written in Python. It includes four game ROMs: Brick Break, Connect 4, Pong, and Tic Tac Toe. The interpreter allows you to play these classic games on your computer.
+
+## Table of Contents
+
+- [CHIP-8 Interpreter in Python](#chip-8-interpreter-in-python)
+  - [Table of Contents](#table-of-contents)
+  - [Introduction](#introduction)
+  - [Features](#features)
+  - [Requirements](#requirements)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Note](#note)
+    - [For PONG use the following keys](#for-pong-use-the-following-keys)
+    - [For other games please find the key on your own](#for-other-games-please-find-the-key-on-your-own)
+
+## Introduction
+
+CHIP-8 is an interpreted programming language that was developed in the 1970s to run simple games on computers. This project is a Python implementation of a CHIP-8 interpreter, allowing you to play four classic games.
+
+## Features
+
+- Emulates CHIP-8 instructions
+- Renders graphics using a simple pixel grid
+- Supports keyboard input for game control
+- Includes four game ROMs:
+  - Brick Break
+  - Connect 4
+  - Pong
+  - Tic Tac Toe
+
+## Requirements
+
+- Python 3.x
+- `pyglet` library (for rendering graphics and handling input)
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/Abhi25102004/Maybe-a-Chip-8-emulator-on-the-terminal-in-Python.git
+   cd Maybe-a-Chip-8-emulator-on-the-terminal-in-Python
+   ```
+
+## Usage
+
+To start the CHIP-8 interpreter and load a game ROM, run the following command:
+
+```bash
+python chip8.py
+```
+
+## Note
+
+### For PONG use the following keys
+
+For **left** pad :
+
+1. `Q` : Up
+2. `1` : Down
+
+For **Right** pad :
+
+1. `R` : Up
+2. `4` : Down
+
+### For other games please find the key on your own
--- chip8.py
+++ chip8.py
@@ -1,10 +1,32 @@
-# basic imports
-import time 
-import os
+# Importing 
+import pyglet
 import random
+import os
+import sys
 
-# chip - 8 variables
-display = [[0 for _ in range(64)] for _ in range(32)]
+# game menu
+game_choice = str()
+while game_choice == str():
+    os.system('cls')
+    print("Choose A Game:\n1. Brick Break\n2. Connect 4\n3. Pong\n4. Tic Tac Toe\n5. To Quit")
+    input_value = int(input("Enter which game to play: "))
+    match input_value:
+        case 1:
+            game_choice = "BRIX"
+        case 2:
+            game_choice = "CONNECT4"
+        case 3:
+            game_choice = "PONG"
+        case 4:
+            game_choice = "TICTAC"
+        case 5:
+            sys.exit(0)
+        case _:
+            game_choice = str()
+    
+
+# Creating variables
+display = [0] *2048
 memory = [0] * 4096
 registers = [0] * 16
 index = 0
@@ -12,19 +34,72 @@ delay_timer = 0
 sound_timer = 0
 program_counter = 0x200
 stack = []
+key_inputs = [0]*16
+fonts = [0xF0, 0x90, 0x90, 0x90, 0xF0, # 0
+           0x20, 0x60, 0x20, 0x20, 0x70, # 1
+           0xF0, 0x10d, 0xF0, 0x80, 0xF0, # 2
+           0xF0, 0x10, 0xF0, 0x10, 0xF0, # 3
+           0x90, 0x90, 0xF0, 0x10, 0x10, # 4
+           0xF0, 0x80, 0xF0, 0x10, 0xF0, # 5
+           0xF0, 0x80, 0xF0, 0x90, 0xF0, # 6
+           0xF0, 0x10, 0x20, 0x40, 0x40, # 7
+           0xF0, 0x90, 0xF0, 0x90, 0xF0, # 8
+           0xF0, 0x90, 0xF0, 0x10, 0xF0, # 9
+           0xF0, 0x90, 0xF0, 0x90, 0x90, # A
+           0xE0, 0x90, 0xE0, 0x90, 0xE0, # B
+           0xF0, 0x80, 0x80, 0x80, 0xF0, # C
+           0xE0, 0x90, 0x90, 0x90, 0xE0, # D
+           0xF0, 0x80, 0xF0, 0x80, 0xF0, # E
+           0xF0, 0x80, 0xF0, 0x80, 0x80  # F
+]
+KEY_MAP = {pyglet.window.key._1: 0x1,
+           pyglet.window.key._2: 0x2,
+           pyglet.window.key._3: 0x3,
+           pyglet.window.key._4: 0xc,
+           pyglet.window.key.Q: 0x4,
+           pyglet.window.key.W: 0x5,
+           pyglet.window.key.E: 0x6,
+           pyglet.window.key.R: 0xd,
+           pyglet.window.key.A: 0x7,
+           pyglet.window.key.S: 0x8,
+           pyglet.window.key.D: 0x9,
+           pyglet.window.key.F: 0xe,
+           pyglet.window.key.Z: 0xa,
+           pyglet.window.key.X: 0,
+           pyglet.window.key.C: 0xb,
+           pyglet.window.key.V: 0xf
+}
+window = pyglet.window.Window(width = 700 , height = 400)
+image = pyglet.resource.image('pixel.png')
+music = pyglet.resource.media('buzz.wav', streaming=False)
 
+# creating batches of image
+batch = pyglet.graphics.Batch()
+sprites = []
+for i in range(2048):
+    sprites.append(pyglet.sprite.Sprite(image,batch=batch))
 
-# feeding values to the memory
-with open("c8_test.c8","rb") as file:
-    data = file.read()
-for info in range(0,len(data)):
-        memory[info + 0x200] = data[info]
+# Adding data into memory
+# Fonts addition
+for i in range(80):
+    memory[i] = fonts[i]
 
-# main cycle
-while True:
-    increment = 2
-    opcode = (memory[program_counter] << 8) | memory[program_counter + 1] 
-    hex_opcode = hex(opcode)
+# Instructions addition
+binary = open(game_choice, "rb").read()
+for i in range(len(binary)):
+    memory[i+0x200] = binary[i]
+
+# Main loop
+@window.event
+def on_draw():
+    global program_counter
+    global delay_timer
+    global sound_timer
+    global index
+    should_draw = False
+
+    # Instruction handling
+    opcode = (memory[program_counter] << 8) | memory[program_counter + 1]
     value = (opcode & 0xf000) >> 12
     vx = (opcode & 0x0f00) >> 8
     vy = (opcode & 0x00f0) >> 4
@@ -33,24 +108,24 @@ while True:
             value = (opcode & 0x000f)
             match value:
                 case 0x0:
-                    display = [[0 for _ in range(64)] for _ in range(32)]
+                    window.clear()
                 case 0xE:
                     if len(stack) != 0:
                         program_counter = stack.pop()
         case 0x1:
             program_counter = (opcode & 0x0fff)
-            increment = 0
+            program_counter -= 2
         case 0x2:
             stack.append(program_counter)
         case 0x3:
             if  registers[vx] == (opcode & 0x00ff):
-                increment = 4
+                program_counter += 2
         case 0x4:
             if  registers[vx] != (opcode & 0x00ff):
-                increment = 4
+                program_counter += 2
         case 0x5:
             if registers[vx] == registers[vy]:
-                increment = 4
+                program_counter += 2
         case 0x6:
             registers[vx] = (opcode & 0x00ff)
         case 0x7:
@@ -98,34 +173,49 @@ while True:
                     registers[vx] = registers[vx] << 1
         case 0x9:
             if registers[vx] != registers[vy]:
-                increment = 4
+                program_counter += 2
         case 0xA:
             index = (opcode & 0x0fff)
         case 0xB:
             program_counter = (opcode & 0x0fff) + registers[0]
-            increment = 0
+            program_counter -= 2
         case 0xC:
             registers[vx] = (random.randint(0,255) & (opcode & 0x00ff))
         case 0xD:
+            registers[0xf] = 0
             for row in range(opcode & 0x000f):
-                sprite = str(bin(memory[index + row]))[2:]
-                for i in range(8):
-                    if registers[vx] + row >= 0 and registers[vx] + row <= 31 and registers[vy] + i >= 0 and registers[vy] + i <= 61: 
-                        display[registers[vx] + row][registers[vy] + i] = int(sprite[i])
+                sprite = memory[index + row]
+                for column in range(8):
+                    if registers[vx] + column >= 64 or registers[vy] + row >= 32:
+                        continue
+                    binary_value = (sprite >> (7 - column)) & 1
+                    if display[registers[vy] + row + (registers[vx] + column) * 32] & 1:
+                        registers[0xf] = 1
+                    display[registers[vy] + row + (registers[vx] + column) * 32] ^= binary_value
+            should_draw = True
         case 0xE:
             value = (opcode & 0x000f)
             match value:
                 case 0xE:
-                    pass
+                    if key_inputs[registers[vx]] == 1:
+                        program_counter += 2
                 case 0x1:
-                    pass
+                    if key_inputs[registers[vx]] == 0:
+                        program_counter += 2
         case 0xF:
             value = (opcode & 0x00ff)
             match value:
                 case 0x07:
-                    registers[vx] = delay_timer 
+                    registers[vx] = delay_timer
                 case 0x0A:
-                    registers[vx] = input("Enter value")
+                    i = 0
+                    while i < 16:
+                        if key_inputs[i] == 1:
+                            registers[vx] = i
+                            break
+                        i += 1
+                    else:
+                        registers[vx] = -1
                 case 0x15:
                     delay_timer = registers[vx]
                 case 0x18:
@@ -133,7 +223,7 @@ while True:
                 case 0x1E:
                     index = index + registers[vx]
                 case 0x29:
-                    pass
+                    index = 5 * registers[vx]
                 case 0x33:
                     number = registers[vx]
                     i = 2
@@ -147,14 +237,41 @@ while True:
                 case 0x65:
                     for i in range(vx+1):
                         registers[i] = memory[i + index]
-    program_counter += increment
-    os.system("cls")
-    for i in display:
-        for j in i:
-            if j == 1:
-                print(" ",end=" ")
+    
+    # Incrementing to next instruction
+    program_counter += 2
+
+    # drawing on the screen
+    if should_draw:
+        for i in range(2048):
+            if display[i] == 1:
+                sprites[i].x = ((i - ( i % 32)) // 32) * 10
+                sprites[i].y = (i % 32) * 10
+                sprites[i].batch = batch
             else:
-                print("O",end=" ")
-        print()
-    print("opcode", hex(opcode))
-    time.sleep(1/60)
+                sprites[i].batch = None
+        window.clear()
+        batch.draw()
+        window.flip()
+        should_draw = False
+
+    # decrementing delay and sound timer
+    if delay_timer > 0 :
+        delay_timer -= 1
+
+    if sound_timer > 0:
+        sound_timer -= 1
+        if sound_timer == 0:
+            music.play()
+
+@window.event
+# checks if any of the keys is pressed or not
+def on_key_press(symbol, modifiers):
+    key_inputs[KEY_MAP[symbol]] = 1
+
+@window.event
+# checks if any of the keys is released or not
+def on_key_release(symbol, modifiers):
+    key_inputs[KEY_MAP[symbol]] = 0
+
+pyglet.app.run()
